d57e3669def7b41b2be788c08f9ba3e7b746b910
dprosenikov <84418982+dprosenikov@users.noreply.github.com>
2021-07-02 17:14:59 +0300

    Delete REST_User directory

--- REST_User/users/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from REST_User.users.views import ProfileList, ProfileCreate, ProfileDetailsApiView
-
-urlpatterns = [
-    path('', ProfileList.as_view(), name='all users'),
-    path('create/', ProfileCreate.as_view(), name='create user'),
-    path('<int:pk>/', ProfileDetailsApiView.as_view(), name='user details'),
-]
